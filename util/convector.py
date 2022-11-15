a8d5cc2284be44e4be373d91503ab9d27027edde
Avill1email <st@itstep.lan>
2022-11-15 21:00:34 +0300

    Initial commit

---
+++ util/convector.py
@@ -0,0 +1,15 @@
+from model.human import Human
+
+class Convertor:
+
+    @staticmethod
+    def convert_to_string(humans):
+        s = "List of humans:\n"
+        if not isinstance(humans, (list, tuple)):
+            return "List is empty"
+
+        s = "List of humans:\n"
+        for human in humans:
+            if isinstance(human, Human):
+                s += "\n" + str(human)
+        return s
